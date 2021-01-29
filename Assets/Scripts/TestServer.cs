cc212a4433337cd261c352520312d1803298255b
JMatachun468 <jared.matachun@aim.com>
2021-01-29 13:12:19 -0500

    Puncher Server Test

--- Assets/Scripts/TestServer.cs
+++ Assets/Scripts/TestServer.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using MLAPI;
 using MLAPI.Spawning;
+using MLAPI.Puncher.Client;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 public class TestServer : MonoBehaviour
 {
-
+    public const string PUNCHER_SERVER_HOST = "18.216.176.155";
+    public const int PUNCHER_SERVER_PORT = 6776;
+    public GameObject ConnectAddress;
     public void StartAsHost()
     {
         NetworkingManager.Singleton.StartHost();
@@ -15,6 +22,22 @@ public class TestServer : MonoBehaviour
 
     public void JoinAsClient()
     {
+        string address = ConnectAddress.GetComponent<Text>().text;
+
+            using (PuncherClient connectPeer = new PuncherClient(PUNCHER_SERVER_HOST, PUNCHER_SERVER_PORT))
+            {
+                System.Console.WriteLine("[CONNECTOR] Punching...");
+
+                if (connectPeer.TryPunch(IPAddress.Parse(address), out IPEndPoint connectResult))
+                {
+                    System.Console.WriteLine("[CONNECTOR] Punched through to peer: " + connectResult);
+                }
+                else
+                {
+                    System.Console.WriteLine("[CONNECTOR] Failed to punch");
+                }
+            }
+        
         NetworkingManager.Singleton.StartClient();
         gameObject.SetActive(false);
     }
