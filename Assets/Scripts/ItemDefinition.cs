0501ec54ab878d7610c3a4c6f57eb526fd5c4c6c
zacharySpitzer <45111466+zacharySpitzer@users.noreply.github.com>
2021-02-02 17:06:06 -0500

    items definition and start of item container

---
+++ Assets/Scripts/ItemDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenuAttribute(fileName = "newItem", menuName = "Atro Items")]
+
+public class ItemDefinition : ScriptableObject
+{
+    public int  itemId=-1;
+    public string ItemName="Default Name";
+    public GameObject ObjectModel;
+    public string itemDescription = "insert Degen Humor Here";
+    public bool hasEffect=false;
+    public bool isContraband= false;
+    public bool isCrafted=false;
+
+    public List<itemEffects> Effects;
+
+    public Containers startingLocation = null;
+  
+}
+
