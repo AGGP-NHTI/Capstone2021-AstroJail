a7b57f87da377cf85aec834a70d23041ca3c7d36
zacharySpitzer <45111466+zacharySpitzer@users.noreply.github.com>
2021-02-02 17:48:42 -0500

    CONTAINER

--- Assets/Scripts/Containers.cs
+++ Assets/Scripts/Containers.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//KNOWN ISSUE :
+//WILL NEED UPDATING UI AND OTHER ENTITIES WHEN CONTENTS CHANGE 
+//THINK LIKE A MINECRAFT CHEST IN MULTIPLAYER. YOU SHOULD BE ABLE TO SEE WHEN CONTENTS CHANGE
 public class Containers : MonoBehaviour
 {
+
+    public int MaxItems = 5;
     public List<ItemDefinition> startingItems;
     public List<ItemDefinition> ItemsInContainer;
-    public int MaxItems = 5;
+    
     // Start is called before the first frame update
     void Start()
     {
         foreach (ItemDefinition item in startingItems)
         {
-
             //create a copy of the item 
-            ItemDefinition newItem = (ItemDefinition)ScriptableObject.CreateInstance(item.GetType());
+            // ItemDefinition newItem = (ItemDefinition)ScriptableObject.CreateInstance(item.GetType());
+            ItemDefinition newItem = Instantiate(item);
 
             //set item copy container to this container
             newItem.startingLocation = this;
@@ -22,12 +28,48 @@ public class Containers : MonoBehaviour
             //put item into container (items in container)
             this.Additem(newItem);
 
-
         }
     }
 
+    /*
+    //this is for debugging 
+    private void Update()
+    {
+         if(Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (ItemDefinition item in ItemsInContainer)
+            {
+                Debug.Log(item.itemId + ": " + item.ItemName);
+            }
+        }       
+    }
+    */
+
+
     public ItemDefinition TakeItem(ItemDefinition item)
     {
+
+        //TO DO : 
+        //verify that the item is still there
+        ItemsInContainer.Remove(item);
+        //return null when you cannot remove the item asked 
+        return null;
+    }
+    public ItemDefinition TakeItem(int itemAt)
+    {
+        //this may not be the version/method we want to use :subject to change
+
+        //TO DO : 
+        //verify that there is an item to be taken at location
+
+        //to do :
+        //need to grab the item out of container before removing it from container 
+        //use this temp reff to return to player to verify this is what they got 
+
+
+        ItemsInContainer.RemoveAt(itemAt);
+        //
+        //return null when you cannot remove the item asked 
         return null;
     }
     
