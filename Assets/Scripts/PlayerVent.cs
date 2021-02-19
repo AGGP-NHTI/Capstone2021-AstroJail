240f13c9dee67ccb955a9d1dba534e5b89311f13
JMatachun468 <jared.matachun@aim.com>
2021-02-19 14:22:23 -0500

    vent test

--- Assets/Scripts/PlayerVent.cs
+++ Assets/Scripts/PlayerVent.cs
@@ -13,6 +13,19 @@ public class PlayerVent : MapInteractable
     // Update is called once per frame
     void Update()
     {
+        if (Label.activeSelf)
+        {
+            Label.transform.rotation = Quaternion.LookRotation(Label.transform.position - Camera.main.transform.position);
+        }
+    }
+
+    public override bool OnUse(PlayerController user)
+    {
+        UsingPlayer = null;
+        PlayerPawn tempPawn = (PlayerPawn)user.myPawn;
+        tempPawn.transform.position = endPoint.transform.position;
+        tempPawn.ObjectUsing = null;
         
+        return true;
     }
 }
