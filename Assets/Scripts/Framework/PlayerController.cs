883110d14d854341d96072c8ee1ac3889ee29b39
zacharySpitzer <zachspitzer98@gmail.com>
2021-04-02 12:37:13 -0400

    big boi push

--- Assets/Scripts/Framework/PlayerController.cs
+++ Assets/Scripts/Framework/PlayerController.cs
@@ -169,7 +169,7 @@ public class PlayerController : Controller
     public void client_set(ulong id)
     {
         Debug.Log("client_set with owner id of " + id);
-        myPawn = GetNetworkedObject(id).GetComponent<PlayerPawn>();
+        myPawn = GetNetworkedObject(id).GetComponent<PrisonerPawn>();
         myPawn.Possessed(this);
         myPawn.CameraControl.SetActive(true);
         myController = true;
