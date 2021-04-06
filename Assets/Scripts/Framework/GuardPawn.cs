c1d091cc0854b9500c1f3c3251b5c8371b814611
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2021-04-06 17:37:24 -0400

    MAP FINISHED

--- Assets/Scripts/Framework/GuardPawn.cs
+++ Assets/Scripts/Framework/GuardPawn.cs
@@ -3,17 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using MLAPI;
+using MLAPI.Connection;
+
 
 
 public class GuardPawn : PlayerPawn
 {
-    
+    public GameObject HUDPanelToAttach;
+    public GameObject HudReference;
+    Containers container;
     Camera theCam;
     Ray locateRay;
     public float searchDistance = 10f;
     public PrisonerPawn FoundPlayer = null;
     public PrisonerPawn searchedPlayer = null;
-   
+    public List<int> toRecieve;
+
 
     //Properties
 
@@ -35,32 +41,35 @@ public class GuardPawn : PlayerPawn
     public void FindPrisoners()
     {
         FoundPlayer = null;
-        locateRay.direction = theCam.transform.forward ;
+        locateRay.direction = theCam.transform.forward;
         //this might need to be moved to a better starting position
         locateRay.origin = gameObject.transform.position;
-        
+
         RaycastHit hitInfo;
 
         if (Physics.Raycast(locateRay, out hitInfo, searchDistance))
         {
-            FoundPlayer = hitInfo.collider.gameObject.GetComponentInParent<PrisonerPawn>();
-           
-            
+            if (hitInfo.collider.gameObject.GetComponentInParent<PrisonerPawn>())
+            {
+                FoundPlayer = hitInfo.collider.gameObject.GetComponentInParent<PrisonerPawn>();
+
+            }
+
         }
 
     }
 
 
     public override void Interact(bool e)
-    {     
-       
+    {
+
         if (e && !ObjectUsing && (Interactables.Count != 0))
         {
             Debug.Log($"pressed e: {e} ");
             if (Interactables[0].IsInUse())
             {
                 Debug.Log("object is in use");
-                
+
             }
             else
             {
@@ -75,11 +84,28 @@ public class GuardPawn : PlayerPawn
     {
         if (q)
         {
-            if(FoundPlayer && !searchedPlayer)
+            if (FoundPlayer && !searchedPlayer)
             {
                 searchedPlayer = FoundPlayer;
                 this.lockMovement = true;
                 searchedPlayer.lockMovement = true;
+
+
+                if (IsServer)
+                {
+                   // InvokeClientRpcOnEveryone(Client_RequestItems);
+                }
+                else
+                {
+                    InvokeServerRpc(Server_RequestItems, searchedPlayer.NetworkId);
+                }
+
+
+                //for me to put in here 
+                //pull up player inventory hud. send found player to hud same as stashed
+                //move logic to hud script
+
+
 
                 // we have done this locally on guard client machine 
                 // we now need to replicate what we have done here acrossed the network 
@@ -96,7 +122,7 @@ public class GuardPawn : PlayerPawn
                 // 1.a) guard can open hud element with prisoner inventory information
                 // 2.) sends client rpc (search begins) to prisoner to inform them being searched and a location
                 // 2.a) prisoner can open hud element and places themselves at location
-          
+
 
 
 
@@ -122,9 +148,20 @@ public class GuardPawn : PlayerPawn
 
             }
         }
-        
+
     }
-    
+    public void ItemsUpdated()
+    {
+        //this creates the itemhud and gives the items in container
+        if (HUDPanelToAttach.GetComponent<SearchPlayerHud>())
+        {
+            HudReference = Instantiate(HUDPanelToAttach);
+           // HudReference.GetComponent<SearchPlayerHud>()._container = container;
+            //HudReference.GetComponent<SearchPlayerHud>()._player = UsingPlayer;
+            //HudReference.GetComponent<SearchPlayerHud>().stash = this;
+        }
+
+    }
 
     public override void Close(bool escape)
     {
@@ -133,15 +170,54 @@ public class GuardPawn : PlayerPawn
         {
             EndInteract();
         }
-        if(escape && searchedPlayer)
+        if (escape && searchedPlayer)
         {
             this.lockMovement = false;
             searchedPlayer.lockMovement = false;
             searchedPlayer = null;
             EndInteract();
         }
-       
+
 
     }
 
+    [ServerRPC(RequireOwnership = false)]
+    public int[] Server_RequestItems(ulong playerID)
+    {
+        int[] test;
+        PrisonerPawn temp = null;
+        foreach (NetworkedClient nc in NetworkingManager.Singleton.ConnectedClientsList)
+        {
+            if (nc.ClientId == playerID)
+            {
+                temp = (PrisonerPawn)nc.PlayerObject.GetComponent<PlayerController>().myPawn;
+            }
+        }
+
+    InvokeClientRpcOnClient(Client_RequestItems,playerID);
+
+        return null;
+    }
+
+    [ClientRPC]
+    public int[] Client_RequestItems()
+    {
+        PrisonerPawn temp = null;
+        List<int> itemIDs = new List<int>();
+        foreach (PlayerController pc in GameObject.FindObjectsOfType<PlayerController>())
+        {
+            if(pc.myController)
+            {
+                temp = (PrisonerPawn)pc.myPawn;
+            }
+        }
+
+        foreach(ItemDefinition id in temp.playerInventory.ItemsInContainer)
+        {
+            itemIDs.Add(id.instanceId);
+        }
+        int[] items = itemIDs.ToArray();
+
+        return items;
+    }
 }
