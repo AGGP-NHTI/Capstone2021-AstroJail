9dbdeeabb0dee311dfebcb3b1c0b15ede7054bf4
JMatachun468 <jared.matachun@aim.com>
2021-04-09 14:59:44 -0400

    test

---
+++ Assets/Scripts/SpawnPoints.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour
+{
+    private static SpawnPoints _instance;
+    public static SpawnPoints Instance { get { return _instance; } }
+
+    public List<Transform> guardSpawns, prisonerSpawns;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
