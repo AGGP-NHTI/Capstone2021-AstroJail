b55067742e01ede47dd2e0f85d43b951511253b7
zacharySpitzer <zachspitzer98@gmail.com>
2021-04-23 12:36:52 -0400

    test on prisoner lock

--- Assets/Scripts/Framework/GuardPawn.cs
+++ Assets/Scripts/Framework/GuardPawn.cs
@@ -153,45 +153,10 @@ public class GuardPawn : PlayerPawn
 
 
                 searchedPlayer.playerInventory.GuardRequestItems(searchedPlayer.OwnerClientId, OwnerClientId);
-                BeginSearchPrisonerClientRpc();
+                BeginSearchPrisonerServerRpc(searchedPlayer.OwnerClientId);
 
-                //for me to put in here 
-                //pull up player inventory hud. send found player to hud same as stashed
-                //move logic to hud script
 
-                // we have done this locally on guard client machine 
-                // we now need to replicate what we have done here acrossed the network 
-                // insync with the player that is being searched 
-                // we may have to tell the prisoner where they need to be standing
-                // they need a hud to show up saying they are being searched 
-                // get there inventory then show the guards search hud with that information 
 
-                // search begins 
-                // Guard sends server rpc and it states which player and their location 
-                // in server rpc 
-                // 1.) sends client rpc (prisoner inventory) to guard with prisoner inventory 
-                // 1.a) guard can open hud element with prisoner inventory information
-                // 2.) sends client rpc (search begins) to prisoner to inform them being searched and a location
-                // 2.a) prisoner can open hud element and places themselves at location
-
-                // the search interaction ends when the guard
-                // either
-                // A.) ends the search without picking an item from inventory
-                // B.) follows through on search
-
-                // if A, then we need to call end interaction on prisoner(across network) and guard
-                // if B, guard wins/ prisoner wins interaction
-
-                // if guard wins
-                // report interaction to all players 
-                // then send items back to starting stash
-                // teleport player to cell 
-                // release both players and remove huds(ends interaction)
-
-                // if Prisoner wins 
-                // report interaction to all players 
-                // Guard enters sleep/cooldown state
-                // release both players and remove huds(ends interaction)  
 
 
             }
@@ -240,54 +205,22 @@ public class GuardPawn : PlayerPawn
 
         this.lockMovement = false;
         searchedPlayer.lockMovement = false;
-        searchedPlayer = null;
         EndSearchPrisonerServerRpc(searchedPlayer.OwnerClientId);
+        searchedPlayer = null;
+        
         EndInteract();
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    public void RequestItemsServerRpc(ulong playerID)
+  [ServerRpc(RequireOwnership =false)]
+  public void BeginSearchPrisonerServerRpc(ulong playerID)
     {
-        ClientRpcParams CRP = new ClientRpcParams();
+         ClientRpcParams CRP = new ClientRpcParams();
         ulong[] targetClientID = new ulong[1];
         targetClientID[0] = playerID;
         CRP.Send.TargetClientIds = targetClientID;
 
-        int[] test;
-        PrisonerPawn temp = null;
-        foreach (NetworkClient nc in NetworkManager.Singleton.ConnectedClientsList)
-        {
-            if (nc.ClientId == playerID)
-            {
-                temp = (PrisonerPawn)nc.PlayerObject.GetComponent<PlayerController>().myPawn;
-            }
-        }
-
         BeginSearchPrisonerClientRpc(CRP);
-        RequestItemsClientRpc(control.myClientParams);
-
     }
-
-    [ClientRpc]
-    public void RequestItemsClientRpc(ClientRpcParams clientRpcParams = default)
-    {
-        PrisonerPawn temp = null;
-        List<int> itemIDs = new List<int>();
-        foreach (PlayerController pc in GameObject.FindObjectsOfType<PlayerController>())
-        {
-            if (pc.myController)
-            {
-                temp = (PrisonerPawn)pc.myPawn;
-            }
-        }
-        foreach (ItemDefinition id in temp.playerInventory.ItemsInContainer)
-        {
-            itemIDs.Add(id.instanceId);
-        }
-        int[] items = itemIDs.ToArray();
-
-    }
-
 
     [ClientRpc]
     public void BeginSearchPrisonerClientRpc(ClientRpcParams clientRpcParams = default)
